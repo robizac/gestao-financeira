34713a81ea7d2f094ca32339c8a93122f6e2f353
Roberto Cardoso <robizac@gmail.com>
2025-06-11 22:33:56 -0300

    corrigir configuracoes banco de dados

--- src/main/java/br/com/roberto/gestaofinanceira/repository/DespesaRepository.java
+++ src/main/java/br/com/roberto/gestaofinanceira/repository/DespesaRepository.java
@@ -1,14 +1,7 @@
 package br.com.roberto.gestaofinanceira.repository;
 
 import br.com.roberto.gestaofinanceira.entity.Despesa;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.UUID;
-
 public interface DespesaRepository extends JpaRepository<Despesa, Long> {
-    Page<Despesa> findAll( Pageable pageable);
 }
